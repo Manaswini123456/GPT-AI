776b1ab41dd2f36a067f428268ddd1941ff22281
Manaswini123456 <janu2001dec@gmail.com>
2023-07-22 10:50:39 +0530

    Complete GPT-AI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       items-center w-full mb-10 pt-3'>
         <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
         <button type='button'
-        onClick={()=> window.open('')} className='black_btn'>
+        onClick={()=> window.open('https://github.com/Manaswini123456/GPT-AI')} className='black_btn'>
           Github
         </button>
       </nav>
