b38787c4ceffb798fb1971c2484bf2b00f8e27fd
Manaswini123456 <janu2001dec@gmail.com>
2023-07-22 11:23:51 +0530

    All

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       <h1 className='head_text'>Summarize Articles with<br className='max-mid:hidden'/>
       <span className='orange_gradient'>OpenAI GPT-4</span></h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer that trnasforms
+        Simplify your reading with Summize, an open-source article summarizer that transforms
         lengthy articles into clear and concize summaries.
       </h2>
     </header>
